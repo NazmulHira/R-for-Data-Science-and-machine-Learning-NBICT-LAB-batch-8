0a79f17c5abda3e30a1e703509f4fa569c1fa727
NazmulHira <nazmul.islam.81@gmail.com>
2025-05-07 22:50:43 +0600

    logical operator

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/rBasic(part-2).R
@@ -0,0 +1,51 @@
+# Data Types in R
+# 1. Numeric -Integer/floating point 
+# 2. string/character data types - "Nazmul"
+# 3. Logical data types
+a=11
+b=7.19
+class(a)
+class(b)
+
+
+
+name="nazmul islam"
+print(name)
+class(name)
+a>b
+
+
+
+c="52"
+print(c+9)
+print(a+9)
+class(c)
+
+class(a<b)
+print(a<b)
+x=19
+y = 22-3
+print(x==y)
+print(y>=x)
+
+
+
+
+#logical operators in R 
+4==4 && 5==5
+# && logical operator
+# F || T
+F || T
+T || T
+#Simple mathematical Operators in R
+z = 6+9-(8*2)/3
+print(z)
+
+
+w= (4+3-3)*2/3
+print(w)
+y = 4^3
+
+print(y)
+11/5
+11%/%5
