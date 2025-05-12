a91e8821bc0df55d2c0b621b2b07a9bb27c3266f
NazmulHira <nazmul.islam.81@gmail.com>
2025-05-12 22:51:33 +0600

    Create day5.4

---
+++ RDSML-Day-5 👉 Vectors in R/day5.R
@@ -0,0 +1,62 @@
+# Vectors in R
+# creating a vector using the c()command
+student.height= c(60, 69, 55, 62)
+student.height
+
+#checking the class of the vector student.height
+is.numeric(student.height)
+is.logical(student.height)
+# R automatically coverts numeric to text, when you a text item in the vector
+b=c(5, 8, 2, "sv")
+b
+is.numeric(b)
+is.character(b)
+
+# we can covert data types 
+# For example from numeric to character 
+a=c(1,2,3,4,5)
+a
+
+class(a)
+is.numeric(a)
+a=as.character(a)
+class(a)
+#logical operator TRUE and FALSE when converted to numeric, TRUE converts to 1
+# and false converts to 0
+d=c(TRUE, FALSE, FALSE, TRUE, FALSE)
+d=as.numeric(d)
+d
+#converting numeric to logical (1 or any number other than 0 converts to true,
+# 0 converts to False )
+d
+classs(d)
+d= as.logical(d)
+d
+ e=c(1, 0, 1, 1, 23, -7,0)
+e=as.logical(e) 
+e
+#creating sequential vector
+# creating a vector starting from 1 to 10
+my_seq =c(1:50,  80, 90, 99, 77, 61, 97)
+my_seq
+
+
+# creating a sequential vector using the seq () function
+new_seq= c(seq(1,10))
+new_seq
+new_seq=c(seq(1,10, by=3))
+new_seq
+#vectors can have character
+names=c("elias", "mahfuza", "ashraf")
+names
+class(names)
+names[3]
+names[2:3]
+
+
+
+#Assigning names to vector values
+my_values =c (4,7,9,11)
+names(my_values)=c("a", "b", "c", "d")
+my_values
+my_values["c"]
